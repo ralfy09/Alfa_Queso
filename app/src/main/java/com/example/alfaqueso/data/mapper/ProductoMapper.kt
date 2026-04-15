0b79f7271b8db69a9a6356c2f8da639e08875e50
Ralfy <ralfyfrias@gmail.com>
2026-04-15 18:46:24 -0400

    Add ProductoMapper to map between Producto, ProductoEntity, ProductoDto, and DetallePedido

---
+++ app/src/main/java/com/example/alfaqueso/data/mapper/ProductoMapper.kt
@@ -0,0 +1,66 @@
+package com.example.alfaqueso.data.mapper
+
+import com.example.alfaqueso.data.local.entities.ProductoEntity
+import com.example.alfaqueso.data.remote.dto.ProductoDto
+import com.example.alfaqueso.domain.model.DetallePedido
+import com.example.alfaqueso.domain.model.Producto
+
+// Para la Base de Datos
+fun ProductoEntity.toDomain(): Producto {
+    return Producto(
+        id = this.id,
+        nombre = this.nombre,
+        precio = this.precio,
+        inventarioDisponible = this.inventarioDisponible,
+        inventario = this.inventario
+    )
+}
+
+// Para Azure
+fun ProductoDto.toEntity(): ProductoEntity {
+    return ProductoEntity(
+        id = this.id,
+        nombre = this.nombre,
+        precio = this.precio,
+        inventario = this.inventario,
+        inventarioDisponible = this.inventario
+    )
+}
+
+fun Producto.toEntity(): ProductoEntity{
+    return ProductoEntity(
+        id = this.id,
+        nombre = this.nombre,
+        precio = this.precio,
+        inventario = this.inventario,
+        inventarioDisponible = this.inventario
+    )
+}
+
+fun ProductoDto.toDomain(): Producto {
+    return Producto(
+        id = this.id,
+        nombre = this.nombre,
+        precio = this.precio,
+        inventario = this.inventario,
+        inventarioDisponible = this.inventario
+    )
+}
+
+fun Producto.toDto(): ProductoDto {
+    return ProductoDto(
+        id = this.id,
+        nombre = this.nombre,
+        precio = this.precio,
+        inventario = this.inventario
+    )
+}
+
+fun Producto.toDetallePedido(cantidad: Int): DetallePedido {
+    return DetallePedido(
+        productoId = this.id,
+        nombreProducto = this.nombre,
+        cantidad = cantidad,
+        precioUnitario = this.precio
+    )
+}
