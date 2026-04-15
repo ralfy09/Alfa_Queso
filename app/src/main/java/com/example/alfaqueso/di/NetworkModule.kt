10c6c404a24305c60da93e414556630dcdf6410c
Ralfy <ralfyfrias@gmail.com>
2026-04-15 18:40:32 -0400

    Add NetworkModule with Retrofit and Moshi initialization for QuesoApi

---
+++ app/src/main/java/com/example/alfaqueso/di/NetworkModule.kt
@@ -0,0 +1,26 @@
+package com.example.alfaqueso.di
+
+import com.example.alfaqueso.data.remote.QuesoApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun proveerQuesoApi(): QuesoApi {
+        return Retrofit.Builder()
+            .baseUrl("https://proyectoquesos.azurewebsites.net/") // Tu servidor en Azure
+            // Como vi que agregaste Moshi en tus dependencias, lo usamos aquí:
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+            .create(QuesoApi::class.java)
+    }
+}
