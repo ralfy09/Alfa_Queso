b5af09f5b883bb25a3ade2a823baa2c917d4b1c7
Ralfy <ralfyfrias@gmail.com>
2026-04-15 14:34:06 -0400

    Add Navigation Compose and Hilt Navigation Compose dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,10 +69,14 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation("androidx.compose.material:material-icons-extended")
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.compose.material3)
     debugImplementation(libs.androidx.ui.tooling)
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
 
     // Lifecycle Compose
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.2")
+
 
     // Room
     implementation("androidx.room:room-runtime:2.6.1")
